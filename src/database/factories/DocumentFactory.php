a347eea31e6d21e251d506247de2a7354442e868
christophercreausat <christopher.creusat@ccicampus.fr>
2023-02-04 17:21:23 +0100

    Added document type within schema. Enhanced seeders.

--- src/database/factories/DocumentFactory.php
+++ src/database/factories/DocumentFactory.php
@@ -16,10 +16,35 @@ class DocumentFactory extends Factory
     public function definition(): array
     {
         $now = now();
+        $types = [
+            'image',
+            'zip',
+            'doc',
+            'video',
+            'unknown'
+        ];
+
+        $extensions = [
+            'jpeg',
+            'jpg',
+            'png',
+            'zip',
+            'xlsx',
+            'csv',
+            'doc',
+            'mp4',
+            'mpeg4',
+            'mp3',
+            'avi',
+            'mov',
+            'pdf'
+        ];
+
         return [
             'id' => $this->faker->unique()->uuid,
             'user_id' => User::all()->random(1)->get(0)->id,
-            'name' => Str::slug($this->faker->text(150)),
+            'name' => $this->faker->slug(3) . '.' . $extensions[$this->faker->numberBetween(0, sizeof($extensions) - 1)],
+            'type' => $types[$this->faker->numberBetween(0, sizeof($types) - 1)],
             'storage_link' => $this->faker->url,
             'size' => $this->faker->numberBetween(1, 4096254),
             'created_at' => $now,
--- src/database/migrations/2023_01_16_124548_create_documents_table.php
+++ src/database/migrations/2023_01_16_124548_create_documents_table.php
@@ -14,6 +14,7 @@ class CreateDocumentsTable extends Migration
             $table->uuid('id')->primary();
             $table->uuid('user_id');
             $table->string('name', 150);
+            $table->string('type', 50);
             $table->text('storage_link');
             $table->integer('size');
             $table->timestamps();
